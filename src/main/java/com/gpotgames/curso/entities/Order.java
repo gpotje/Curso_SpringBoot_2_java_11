933f1adfc3de614ea8744ba7a5f8490393a0c662
GPot <gpotje.souza@gmail.com>
2020-08-28 20:02:42 -0300

    OrderStatus enum

--- src/main/java/com/gpotgames/curso/entities/Order.java
+++ src/main/java/com/gpotgames/curso/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gpotgames.curso.entities.enums.OrderStatus;
 
 
 @Entity
@@ -29,6 +30,9 @@ public class Order  implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'" , timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -37,10 +41,11 @@ public class Order  implements Serializable{
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus,User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -66,6 +71,16 @@ public class Order  implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+		
+		this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
