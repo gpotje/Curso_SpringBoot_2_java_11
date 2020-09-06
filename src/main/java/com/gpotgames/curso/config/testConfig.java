54927cbc10d24c98d9ff2fdd76969b605c9f223a
GPot <gpotje.souza@gmail.com>
2020-09-06 19:35:08 -0300

    Payment, one-to-one association

--- src/main/java/com/gpotgames/curso/config/testConfig.java
+++ src/main/java/com/gpotgames/curso/config/testConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.gpotgames.curso.entities.Category;
 import com.gpotgames.curso.entities.OrdemItem;
 import com.gpotgames.curso.entities.Order;
+import com.gpotgames.curso.entities.Payment;
 import com.gpotgames.curso.entities.Product;
 import com.gpotgames.curso.entities.User;
 import com.gpotgames.curso.entities.enums.OrderStatus;
@@ -83,6 +84,11 @@ public class testConfig implements CommandLineRunner {
 		
 		OrderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		
+		Payment pya1 =  new Payment(null,  Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pya1);
+		
+		orderRepository.save(o1);
 	}
 
 	
--- src/main/java/com/gpotgames/curso/entities/Order.java
+++ src/main/java/com/gpotgames/curso/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -33,12 +35,16 @@ public class Order  implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'" , timezone = "GMT")
 	private Instant moment;
 	
+	
 	private Integer orderStatus;
 	
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	
 	
@@ -75,6 +81,14 @@ public class Order  implements Serializable{
 
 	public User getClient() {
 		return client;
+	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public void setClient(User client) {
